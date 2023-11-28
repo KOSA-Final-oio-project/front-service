48a55f8ff2c51fb98bc302001788724b0c34ddb1
sengna <jeonseino_o@naver.com>
2023-11-28 21:04:53 +0900

    feat: vue 코드 틀 작성

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '@/pages/Home.vue';
-// import ChatList from '@/pages/ChatList.vue'
-// import Chats from '@/pages/Chats.vue'
+import Chats from '@/pages/Chats.vue';
 
 const router = createRouter({
-    history: createWebHistory(), // 라우팅 모드 설정
-    // 라우터에 등록할 컴포넌트들
+    history: createWebHistory(),
+
     routes: [
-        { path: '/', component: Home },
-        // { path: '/login', component: Login },
+        {
+            path: '/',
+            name: 'Home',
+            component: Home,
+        },
+        {
+            path: '/chats',
+            name: 'Chats',
+            component: Chats,
+        },
     ],
 });
 
