8334cbbb9bf52d107a1e253a9b82eadd4c1e5ecc
sengna <jeonseino_o@naver.com>
2023-11-28 20:34:07 +0900

    feat: vue 설정 파일 및 폴더 구조 추가

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>🐹Home🐹</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+* {
+    box-sizing: border-box;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from '@/pages/Home.vue'
+// import ChatList from '@/pages/ChatList.vue'
+// import Chats from '@/pages/Chats.vue'
+
+const router = createRouter({
+    history: createWebHistory(), // 라우팅 모드 설정
+    // 라우터에 등록할 컴포넌트들
+    routes: [
+        { path: '/', component: Home },
+        // { path: '/login', component: Login },
+    ]
+})
+
+export default router;
