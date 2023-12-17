41d7d9dd7a792d8eb2a138dbc06647c6ff3e2ad1
jaseongKim <jaseong4281@gmail.com>
2023-12-17 15:52:42 +0900

    feat: post

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ library.add(fas)
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
-import axios from 'axios'
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
